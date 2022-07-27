4360bfabf6b54c2ce347e112d61a025bf3ac2eb4
SnoozingFish <khawley1231@gmail.com>
2022-07-26 22:37:04 -0400

    Link to editor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Crossword V3</title>
-        <meta name="description" content="Edit and share crosswords" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Crossword V3</title>
+				<meta name="description" content="Edit and share crosswords" />
+				{/* <link rel="icon" href="/favicon.ico" /> */}
+			</Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
-        <div>Hello, crossworld.</div>
-      </main>
-    </>
-  );
+			<main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
+				<div>Hello, crossworld.</div>
+				<Link href={"/editor"}>Editor</Link>
+			</main>
+		</>
+	);
 };
 
 export default Home;
