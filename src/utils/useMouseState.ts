6b6e76c958215eb2863aa41dc6a54f4eba4f9524
SnoozingFish <khawley1231@gmail.com>
2022-07-29 19:03:30 -0400

    Custom hook for mouse clicks

---
+++ src/utils/useMouseState.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useMouseState = () => {
+	const [isMouseDown, setMouseDown] = useState(false);
+
+	const setPrimaryButtonState = (e: MouseEvent) => {
+		setMouseDown((e.buttons & 1) === 1);
+	};
+
+	useEffect(() => {
+		window.addEventListener("mousedown", setPrimaryButtonState);
+		window.addEventListener("mousemove", setPrimaryButtonState);
+		window.addEventListener("mouseup", setPrimaryButtonState);
+	});
+
+	return { isMouseDown };
+};
