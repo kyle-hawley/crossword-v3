63cc7b591813428d0b870ec01d5c09deb4e8255b
SnoozingFish <khawley1231@gmail.com>
2022-07-29 19:58:57 -0400

    New font Quicksand

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        15: "repeat(15, minmax(0, 1fr))",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				quicksand: ["Quicksand", "sans-serif"],
+			},
+
+			gridTemplateColumns: {
+				15: "repeat(15, minmax(0, 1fr))",
+			},
+		},
+	},
+	plugins: [],
 };
